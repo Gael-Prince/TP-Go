43fb1f51ed757da820d2079ffb50056eddd453a8
Ben-Gael <gael.prince@eleves.ec-nantes.fr>
2015-12-03 11:33:12 +0100

    ajout du main.cpp

---
+++ main.cpp
@@ -0,0 +1,60 @@
+
+#include "gtest/gtest.h"
+
+#include "fonctions.h"
+#include "classes.h"
+
+
+int addition (int a, int b)
+{
+return a+b;
+}
+
+TEST(SquareRootTest, PositiveNos) {
+    EXPECT_EQ (3, addition (1,2));
+
+}
+
+
+int main(int argc, char **argv) {
+
+goban goban1 ;
+int joueur=1;
+int x,y;
+
+
+
+while(1)
+{
+
+do
+{
+cin >> x >> y ;
+x-= 1;
+y-= 1;
+}
+while (!goban1.PlaceLibre(x,y));
+
+goban1.PoserPierre(joueur, x , y);
+
+
+if (joueur == 1)
+joueur =2;
+else
+joueur =1;
+
+goban1.LibertePierre();
+
+if (!goban1.plateau[2][2].empty())
+ cout << goban1.plateau[2][2][0].liberte << endl;
+
+ goban1.AfficheGoban();
+
+}
+
+
+
+
+  /*testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();*/
+}
