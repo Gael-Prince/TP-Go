8b9adf630fc389a2b291063669bd7cf2b35f0097
fgavel <florian.gavel@orange.fr>
2015-12-10 11:28:32 +0100

    ajout des commentaires

---
+++ classes.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include "gtest/gtest.h"
+using namespace std;
+
+// fichier contenant toutes les classes : pierre, goban et une classe de test sur le goban (pas de tests nécessaires sur le goban)
+
+#ifndef MesClassesDuJeuDeGo_h
+#define MesClassesDuJeuDeGo_h
+
+class pierre
+{ // une pierre possède une couleur et un nombre de libertés, que l'on peut récupérer et initialiser
+protected :
+char couleur;
+int liberte;
+
+public :
+
+char getCouleur (){return couleur;}
+void setCouleur (char col) {couleur = col;}
+int getLiberte (){return liberte;}
+void setLiberte (int lib){liberte = lib;}
+
+
+};
+
+
+class goban
+{ // le plateau est un tableau de vecteur de pierres car cela permet d'avoir des cases vides
+public :
+vector<pierre> plateau [5][5];
+
+public :
+void PoserPierre (int joueur, int x, int y); // pose une pierre au coordonnées x et y de la couleur du joueur
+void LibertePierre (); // calcule le nombre de libertés de la pierre et la supprime si plus de liberté
+void AfficheGoban ();
+bool PlaceLibre (int x, int y);
+
+};
+
+// classe pour faire des test sur le goban (pose de pierre, calcule de libertés, place libre)
+class gobantest : public ::testing::Test
+{
+
+protected :
+  goban goban_test;
+
+virtual void setUp(){}
+virtual void TearDown(){}
+
+};
+
+
+#endif // MesClassesDuJeuDeGo_h
---
+++ main.cpp
@@ -0,0 +1,79 @@
+
+#include "gtest/gtest.h"
+
+#include "fonctions.h"
+#include "classes.h"
+
+
+
+// test permettant de vérifier qu'on ne peut placer une pierre en dehors du goban
+
+TEST_F(gobantest, enDehorsDuTableauTest)
+{
+EXPECT_FALSE (goban_test.PlaceLibre(6,6));
+EXPECT_FALSE (goban_test.PlaceLibre(-1,-1));
+EXPECT_FALSE (goban_test.PlaceLibre(-1,4));
+EXPECT_TRUE (goban_test.PlaceLibre(1,1));
+
+}
+
+// test permettant de vérifier que lorsqu'on on joue une pierre, elle est effectivement posée et que la couleur de cette pierre est bien la couleur du joueur
+
+TEST_F(gobantest, PoseDePierreTest)
+{
+goban_test.PoserPierre (1,2,2);
+EXPECT_FALSE (goban_test.PlaceLibre(2,2));
+EXPECT_EQ ('N', goban_test.plateau[2][2][0].getCouleur());
+goban_test.PoserPierre (2,3,3);
+EXPECT_FALSE (goban_test.PlaceLibre(3,3));
+EXPECT_EQ ('B', goban_test.plateau[3][3][0].getCouleur());
+}
+
+
+
+
+
+int main(int argc, char **argv) {
+
+goban goban1 ;
+int joueur=1;
+int x,y;
+
+
+testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+
+
+while(1)
+{
+
+do
+{
+// on demande l'endroit où le joueur veut poser la pierre
+cin >> x >> y ;
+
+}
+while (!goban1.PlaceLibre(x,y)); // on recommence tant que la place n'est pas disponible
+
+// lorsque le joueur propose une place libre, on pose la pierre
+goban1.PoserPierre(joueur, x , y);
+
+// alternance des tours
+if (joueur == 1)
+joueur =2;
+else
+joueur =1;
+
+// on met à jour les libertés -> permet de supprimer la pierre si elle est encerclée (ne marche pas si un groupe de pierre est encerclé)
+goban1.LibertePierre();
+
+// on affihe le goban
+goban1.AfficheGoban();
+
+}
+
+
+
+
+
+}
